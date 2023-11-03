5da3f0c06c616640cf71fcc3ff442d71b7b63863
KobeDR <kobederidder@MacBook-Pro-van-Kobe.local>
2023-11-03 15:12:41 +0100

    .

--- Scripts/IC_deconvolution_workflow_part2.r
+++ Scripts/IC_deconvolution_workflow_part2.r
@@ -226,7 +226,7 @@ for (u in normalizations){
   
   #Meth_atlas------
   deconv = 'meth_atlas'
-  predicted_cell_proportions <- read.csv(paste(method, '_results_meth_atl.csv', sep = ''))
+  predicted_cell_proportions <- read.csv(paste('Cache/',method, '_results_meth_atl.csv', sep = ''))
   rownames(predicted_cell_proportions) <- predicted_cell_proportions$X
   predicted_cell_proportions$X <- NULL
   
