5e8cb80e6bc69a02c0a8273b66f468c264abc528
KobeDR <kobederidder@MacBook-Pro-van-Kobe.local>
2023-11-03 18:44:27 +0100

    .

--- Run_deconv.sh
+++ Run_deconv.sh
@@ -9,5 +9,5 @@ p_val_cutoff=$4
 n_markers=$5
 
 Rscript ./Scripts/IC_deconvolution_workflow_part1.r $ref_path $val_path $props_path $p_val_cutoff $n_markers
-python ./Scripts/PerformMethatlas.py $props_path
+python3 ./Scripts/PerformMethatlas.py $props_path
 Rscript ./Scripts/IC_deconvolution_workflow_part2.r $props_path
